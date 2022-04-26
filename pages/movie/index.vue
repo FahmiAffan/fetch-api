7d809646a454413f382304d898e8d7cddc18f1f3
Fahmi Affan <fahmiaffan07@gmail.com>
2022-04-26 08:37:44 +0700

    fahmi movie pushed

---
+++ pages/movie/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>Movies</h1>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  data()
+  {
+    movies: []
+  },
+  mounted(){
+    axios.get('http://localhost:3000/movie')
+    .then((response) => console.log(response.data))
+    .catch(err => console.log(err))
+    // .catch(err => console.log(err))
+    // fetch('http://localhost:3000/movie')
+    // .then(res => res.json)
+    // .then(data => this.movie = data)
+    // .catch(err => console.log(err.massage))
+  }
+}
+</script>
+
+<style>
+
+</style>
